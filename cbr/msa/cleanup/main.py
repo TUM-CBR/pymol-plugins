c0387da55ab96084984f2fa5a8ac9e025eafc5d0
Ernesto Rodriguez <ernesto.rodriguez@tum.de>
2023-10-26 14:09:11 +0000

    Basic algorithm to score sequences by the number of gaps they introduce

---
+++ cbr/msa/cleanup/main.py
@@ -0,0 +1,26 @@
+from Bio.Align import MultipleSeqAlignment, SeqRecord
+from typing import List
+
+from ...clustal import msa
+
+def gaps_by_position(alignment : MultipleSeqAlignment) -> List[int]:
+    positions = alignment.get_alignment_length()
+    result = [0 for _ in range(0, positions)]
+
+    for sequence in alignment:
+        for (i, c) in enumerate(sequence):
+            if msa.is_blank(c):
+                result[i] += 1
+
+    return result
+
+def score_by_gap_divergence(alignment : MultipleSeqAlignment) -> List[int]:
+    score_by_position = gaps_by_position(alignment)
+
+    def score_sequence(seq : SeqRecord) -> int:
+        return sum(
+            score_by_position[i]
+            for (i, c) in enumerate(seq) if not msa.is_blank(c)
+        )
+
+    return [score_sequence(s) for s in alignment]
